dd62157c35f903760bfc431b312b9cf4349f0a15
hoang98long <53008046+hoang98long@users.noreply.github.com>
2024-08-06 23:04:35 +0700

    Update avt_preprocess.sh

--- avt_preprocess.sh
+++ avt_preprocess.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if [ -z "$1" ]
+then
+  echo "Usage: $0 <CONFIG_FILE>"
+  exit 1
+fi
+
 CONFIG_FILE=$1
 
 # shellcheck disable=SC2164
 cd /home/avt/github/avt_preprocess
+# shellcheck disable=SC1090
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate avt
 python main.py --config_file "$CONFIG_FILE"
